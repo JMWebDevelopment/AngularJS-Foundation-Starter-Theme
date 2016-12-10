b685a688079edf90bc4da350aff7326e2568f7c5
ViewFromTheBox <jacob.martella@att.net>
2016-12-10 12:13:14 -0600

    Cleaned up the functions.php file

--- functions.php
+++ functions.php
@@ -9,14 +9,15 @@
 /**
  * Table of Contents
  * I. General Functions
- * II. Header Functions
- * III. Home Functions
- * IV. Footer Functions
- * V. Single Post Functions
- * VI. Archive Functions
- * VII. Author Functions
- * VIII. Comments Functions
- * IX. Other Functions
+ * II. WP REST API Functions
+ * III. Header Functions
+ * IV. Home Functions
+ * V. Footer Functions
+ * VI. Single Post Functions
+ * VII. Archive Functions
+ * VIII. Author Functions
+ * IX. Comments Functions
+ * X. Other Functions
  */
 /**
  ******************** I. General Functions *********************************
@@ -141,19 +142,12 @@ function theme_slug_register_sidebars() {
 	));
 }
 add_action( 'widgets_init', 'theme_slug_register_sidebars' );
-/**
- * Add support to query posts by year, month and day
- * @param $valid_vars
- * @return array
- */
-function theme_slug_rest_query_vars( $valid_vars ) {
-	$valid_vars = array_merge( $valid_vars, array( 'author_slug' ) );
-	return $valid_vars;
-}
-add_filter( 'rest_query_vars', 'theme_slug_rest_query_vars' );
 
 /**
- * Rewrite the search url so that we can grab it in the JS
+ ******************** II. WP REST API Functions *********************************
+ */
+/**
+ * Rewrite the search url so that AngularJS can grab it to display results
  */
 function search_url_rewrite () {
 	if ( is_search() && !empty( $_GET['s'] ) ) {
@@ -163,13 +157,12 @@ function search_url_rewrite () {
 }
 add_action( 'template_redirect', 'search_url_rewrite' );
 
+/**
+ * Register the necessary fields to display post information correctly
+ */
 add_action( 'rest_api_init', 'theme_slug_register_rest_fields' );
 function theme_slug_register_rest_fields() {
 
-    register_rest_route( 'wp/v2', '/posts?year=(?P<year>[0-9 .\-]+)', array(
-        'methods' => 'GET',
-        'callback' => 'theme_slug_get_year_posts',
-    ), true );
 
     register_rest_field( 'post',
         'featured_image',
@@ -209,29 +202,21 @@ function theme_slug_register_rest_fields() {
 	register_rest_field( 'post',
 		'author_slug',
 		array(
-			'get_callback' 	  => 'theme_slug_get_author_id',
+			'get_callback' 	  => 'theme_slug_get_author_slug',
 			'update_callback' => null,
 			'schema' 		  => null,
 	) );
 
 }
 
-function theme_slug_get_year_posts( $data ) {
-    echo 'in here somewhere';
-    $posts = get_posts( array ( 'year' => (int) $data[ 'year' ] ) );
-
-    print_r($data);
-
-    if ( empty( $posts ) ) {
-        return null;
-    }
-
-    $response = new WP_REST_Response( $posts );
-
-    return $response;
-}
-
-function theme_slug_get_thumbnail_url($post){
+/**
+ * Grabs the URL of the post's featured image to display it on the page
+ *
+ * @param $post
+ *
+ * @return bool | string, url to the post's featured image
+ */
+function theme_slug_get_thumbnail_url( $post ){
     if ( has_post_thumbnail( $post[ 'id' ] ) ) {
         $imgArray = wp_get_attachment_image_src( get_post_thumbnail_id( $post[ 'id' ] ), 'full' );
         $imgURL = $imgArray[ 0 ];
@@ -241,6 +226,17 @@ function theme_slug_get_thumbnail_url($post){
     }
 }
 
+/**
+ * Returns the array of categories a post is filed under
+ *
+ * @param $object
+ *
+ * @param $field_name
+ *
+ * @param $request
+ *
+ * @return array, list of category names for a post
+ */
 function theme_slug_get_category_name_from_restapi( $object, $field_name, $request ) {
     $cats = [];
     foreach ( $object[ 'categories' ] as $cat ) {
@@ -249,6 +245,17 @@ function theme_slug_get_category_name_from_restapi( $object, $field_name, $reque
     return $cats;
 }
 
+/**
+ * Returns the array of tags a post has
+ *
+ * @param $object
+ *
+ * @param $field_name
+ *
+ * @param $request
+ *
+ * @return array, list of tags for a post
+ */
 function theme_slug_get_tag_name_from_restapi( $object, $field_name, $request ) {
     $tags = [];
     if ( isset( $object[ 'tags' ] ) ) {
@@ -260,7 +267,14 @@ function theme_slug_get_tag_name_from_restapi( $object, $field_name, $request ) 
     return $tags;
 }
 
-add_action( 'rest_post_collection_params', function( $params ) {
+/**
+ * Adds year, month and day parameters to the default collection for the REST API
+ *
+ * @param $params
+ *
+ * @return mixed, the new set of parameters for the collection
+ */
+function theme_slug_rest_post_collection_params( $params ) {
     $params['year'] = array(
         'type'        => 'integer',
         'description' => 'Restrict posts to ones published in a specific year.'
@@ -273,10 +287,20 @@ add_action( 'rest_post_collection_params', function( $params ) {
         'type'        => 'integer',
         'description' => 'Restrict posts to ones published in a specific day.'
     );
-        return $params;
-} );
+    return $params;
+}
+add_action( 'rest_post_collection_params', 'theme_slug_rest_post_collection_params' );
 
-add_filter( 'rest_post_query', function( $query_vars, $request ) {
+/**
+ * Adds the year, month and day parameters to the list of query vars for the REST API
+ *
+ * @param $query_vars
+ *
+ * @param $request
+ *
+ * @return mixed, the new set of query vars with the added vars
+ */
+function theme_slug_rest_post_query ( $query_vars, $request ) {
     if ( $request['year'] ) {
         $query_vars['year'] = $request['year'];
     }
@@ -287,35 +311,45 @@ add_filter( 'rest_post_query', function( $query_vars, $request ) {
         $query_vars['day'] = $request['day'];
     }
         return $query_vars;
-}, 10, 2 );
+}
+add_filter( 'rest_post_query', 'theme_slug_rest_post_query', 10, 2 );
 
+/**
+ * Returns the array of comments for a given post
+ *
+ * @param $object
+ *
+ * @param $field_name
+ *
+ * @param $request
+ *
+ * @return mixed, list of comments for a post
+ */
 function theme_slug_get_comments( $object, $field_name, $request ) {
 
     return get_comments( array( 'post_id' => $object[ 'id' ] ) );
 
 }
 
-function theme_slug_get_post_by_slug( WP_REST_Request $request ) {
-
-	$slug = $request['slug'];
-	$return['slug'] = $slug;
-
-	$return['post'] = get_page_by_path( $slug, ARRAY_A, 'post' );
-	$return['post']['comments'] = get_comments( array( 'ID' => $return['post']['ID'] ) );
-
-	$response = new WP_REST_Response( $return );
-	return $response;
-
-}
-
-function theme_slug_get_author_id( $object, $field_name, $request ) {
+/**
+ * Returns the slug of an author from the given author id
+ *
+ * @param $object
+ *
+ * @param $field_name
+ *
+ * @param $request
+ *
+ * @return string, returns the slug of an author from id
+ */
+function theme_slug_get_author_slug( $object, $field_name, $request ) {
 	$id = $object[ 'author' ];
 	$user = get_user_by( 'id', $id );
 	return $user->user_login;
 }
 
 /**
- ******************** II. Header Functions *********************************
+ ******************** III. Header Functions *********************************
  */
 /**
  * Register Menus
@@ -336,113 +370,23 @@ function theme_slug_add_menu_atts( $atts, $item, $args ) {
 }
 add_filter( 'nav_menu_link_attributes', 'theme_slug_add_menu_atts', 10, 3 );
 /**
- ******************** III. Home Functions *********************************
+ ******************** IV. Home Functions *********************************
  */
 /**
- ******************** IV. Footer Functions *********************************
+ ******************** V. Footer Functions *********************************
  */
 /**
- ******************** V. Single Post Functions *********************************
+ ******************** VI. Single Post Functions *********************************
  */
 /**
- ******************** VI. Archive Functions *********************************
+ ******************** VII. Archive Functions *********************************
  */
 /**
- * Numeric Archive Page Navigation
- */
-function theme_slug_page_navi($before = '', $after = '') {
-	global $wpdb, $wp_query;
-	$request = $wp_query->request;
-	$posts_per_page = intval(get_query_var('posts_per_page'));
-	$paged = intval(get_query_var('paged'));
-	$numposts = $wp_query->found_posts;
-	$max_page = $wp_query->max_num_pages;
-	if ( $numposts <= $posts_per_page ) { return; }
-	if(empty($paged) || $paged == 0) {
-		$paged = 1;
-	}
-	$pages_to_show = 7;
-	$pages_to_show_minus_1 = $pages_to_show-1;
-	$half_page_start = floor($pages_to_show_minus_1/2);
-	$half_page_end = ceil($pages_to_show_minus_1/2);
-	$start_page = $paged - $half_page_start;
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	$end_page = $paged + $half_page_end;
-	if(($end_page - $start_page) != $pages_to_show_minus_1) {
-		$end_page = $start_page + $pages_to_show_minus_1;
-	}
-	if($end_page > $max_page) {
-		$start_page = $max_page - $pages_to_show_minus_1;
-		$end_page = $max_page;
-	}
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	echo $before.'<nav class="page-navigation"><ul class="pagination">'."";
-	if ($start_page >= 2 && $pages_to_show < $max_page) {
-		$first_page_text = __( "First", 'theme-slug' );
-		echo '<li><a href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
-	}
-	echo '<li>';
-	previous_posts_link('Previous');
-	echo '</li>';
-	for($i = $start_page; $i  <= $end_page; $i++) {
-		if($i == $paged) {
-			echo '<li class="current"> '.$i.' </li>';
-		} else {
-			echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
-		}
-	}
-	echo '<li>';
-	next_posts_link('Next');
-	echo '</li>';
-	if ($end_page < $max_page) {
-		$last_page_text = __( "Last", 'theme-slug' );
-		echo '<li><a href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
-	}
-	echo '</ul></nav>'.$after."";
-}
-/**
- ******************** VII. Author Functions *********************************
+ ******************** VIII. Author Functions *********************************
  */
 /**
- ******************** VIII. Comments Functions *********************************
+ ******************** IX. Comments Functions *********************************
  */
-function theme_slug_comments($comment, $args, $depth) {
-	$GLOBALS['comment'] = $comment; ?>
-<li <?php comment_class('panel'); ?>>
-	<div class="media-object">
-		<div class="media-object-section">
-			<?php echo get_avatar( $comment, 75 ); ?>
-		</div>
-		<div class="media-object-section">
-			<article id="comment-<?php comment_ID(); ?>" class="clearfix large-12 columns">
-				<header class="comment-author">
-					<?php
-					// create variable
-					$bgauthemail = get_comment_author_email();
-					?>
-					<?php printf(__('%s', 'theme-slug'), get_comment_author_link()) ?> on
-					<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__(' F jS, Y - g:ia', 'theme-slug')); ?> </a></time>
-					<?php edit_comment_link(__('(Edit)', 'theme-slug'),'  ','') ?>
-				</header>
-				<?php if ($comment->comment_approved == '0') : ?>
-					<div class="alert alert-info">
-						<p><?php _e('Your comment is awaiting moderation.', 'theme-slug') ?></p>
-					</div>
-				<?php endif; ?>
-				<section class="comment_content clearfix">
-					<?php comment_text() ?>
-				</section>
-				<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-			</article>
-		</div>
-	</div>
-	<!-- </li> is added by WordPress automatically -->
-	<?php
-}
 /**
- ******************** IX. Other Functions *********************************
+ ******************** X. Other Functions *********************************
  */
